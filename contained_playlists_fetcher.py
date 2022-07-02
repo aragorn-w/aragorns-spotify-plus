379da23f5cccb16a943517231773b0de9c764bef
Aragorn Wang <wangaragorn@gmail.com>
2022-07-02 00:13:42 -0600

    fff

--- contained_playlists_fetcher.py
+++ contained_playlists_fetcher.py
@@ -6,7 +6,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID_1'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET_1'))
+client_credentials_manager = SpotifyClientCredentials(client_id=os.getenv('SPOTIPY_CLIENT_ID_CONTAINED_PLAYLISTS_FETCHER'), client_secret=os.getenv('SPOTIPY_CLIENT_SECRET_CONTAINED_PLAYLISTS_FETCHER'))
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 library_account = os.getenv('SPOTIFY_PLUS_SECONDARY_ACCOUNT_USER_ID')
@@ -15,13 +15,8 @@ playlists = sp.user_playlists(library_account)
 
 while playlists:
     for i, playlist in enumerate(playlists['items']):
-        print(
-            "%4d %s %s" %
-            (i +
-             1 +
-             playlists['offset'],
-             playlist['uri'],
-             playlist['name']))
+        print("%4d %s %s" %
+            (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
     if playlists['next']:
         playlists = sp.next(playlists)
     else:
