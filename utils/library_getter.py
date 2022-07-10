ca8347aa544c3fe239c076241bf512895e42997c
Aragorn Wang <wangaragorn@gmail.com>
2022-07-10 06:00:28 -0600

    ff

--- utils/library_getter.py
+++ utils/library_getter.py
@@ -18,27 +18,27 @@ def get_simplified_tracks(playlist_id: str):
     simplified_tracks = []
 
     try:
-        current_page = globals.SPOTIFY_API.playlist_items(f'spotify:playlist:{playlist_id}', fields='items.track.id,items.track.is_local,items.track.name,next')
+        current_page = globals.SPOTIFY_API.playlist_items(f"spotify:playlist:{playlist_id}", fields="items.track.id,items.track.is_local,items.track.name,next")
     except SpotifyException as e:
         globals.HANDLE_SPOTIFY_EXCEPTION(e)
 
     while current_page:
-        for item in current_page['items']:
-            track = item['track']
+        for item in current_page["items"]:
+            track = item["track"]
             new_track = {}
 
-            new_track['is_local'] = track['is_local']
+            new_track["is_local"] = track["is_local"]
 
-            if not track['is_local']:
-                new_track['id'] = track['id']
+            if not track["is_local"]:
+                new_track["id"] = track["id"]
             else:
-                new_track['id'] = None
+                new_track["id"] = None
             
-            new_track['name'] = track['name']
+            new_track["name"] = track["name"]
             
             simplified_tracks.append(new_track)
 
-        if current_page['next']:
+        if current_page["next"]:
             try:
                 current_page = globals.SPOTIFY_API.next(current_page)
             except SpotifyException as e:
